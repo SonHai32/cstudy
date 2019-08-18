fa8a773d60f0e219fd8b8778376dd81d0ec86b89
lamsonhai1379 <lamsonhai1379@gmail.com>
2019-08-17 23:47:32 -0400

    add firebase to create user add create handleSubmit and handleError as Input form

---
+++ src/components/auth/register.js
@@ -0,0 +1,131 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {Grid,Icon,Header,Segment, Form, Button,Message, Input, Select} from 'semantic-ui-react'
+import firebase from '../../firebase'
+
+class Register extends React.Component{
+
+    state={
+        email: '',
+        password:'',
+        errors: [],
+        isLoading: false
+
+
+    }
+
+    handleChange = event =>{
+        this.setState({[event.target.name]: event.target.value})
+        this.isFormValid();    
+    }
+
+    handleSubmit = event =>{
+        event.preventDefault();
+       if(this.isFormValid){
+        this.setState({isLoading: true})
+        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(() => {
+            this.setState({isLoading: false})
+        })
+        .catch(err =>{
+            console.log(err)
+            this.setState({errors: this.state.errors.concat(err) ,isLoading: false})
+        })
+       }
+    }
+
+    isPasswordValid = ({password}) =>{
+        if(password.length < 5 ){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+
+    isFormEmpty = ({email,password}) =>{
+        return !email.length || !password.length
+    }
+
+
+    isFormValid = () =>{
+        let error;
+        let errors= [];
+
+        if(this.isFormEmpty(this.state)){
+            return false;
+        }else if(!this.isPasswordValid(this.state)){
+            return false;
+        }else{
+            this.setState({errors: []});
+            return true;
+        }
+    }
+    
+    displayError = (errors, inputName) => {
+        let haveErrorMessage;
+        errors.some(error => error.message.toLowerCase().includes(inputName) ? haveErrorMessage={content: error.message, pointing: 'below'} : haveErrorMessage=false)
+        return haveErrorMessage;
+    }
+
+    render(){
+        const {email, password, errors, isLoading} = this.state;
+        return(
+        
+            <Grid verticalAlign="middle" textAlign="center" className='register'>
+                <Grid.Column style={{maxWidth: 550}}>
+                    <Header color='green' textAlign='center' as='h2'>
+                        Register for CStudy
+                    </Header>
+                    <Form size='large' onSubmit={this.handleSubmit} >
+                        <Segment stacked>
+                            <Form.Input
+                                name='email'
+                                placeholder='Emal Address'
+                                fluid
+                                required
+                                error={this.displayError(errors, 'email')}
+                                size='large'
+                                type='text'
+                                icon='mail'
+                                iconPosition='left'
+                                onChange={this.handleChange}
+                                value={email}
+                            />
+                            <Form.Input 
+                                name='password'
+                                placeholder='Password'
+                                fluid
+                                required
+                                error={this.displayError(errors, 'password')}
+                                size='large'
+                                type='password'
+                                icon='key'
+                                iconPosition='left'
+                                onChange={this.handleChange}
+                                value={password}
+                            />
+                            <Button 
+                            size='large' 
+                            fluid 
+                            color='green'
+                            loading={isLoading}
+                            >
+                                Submit
+                            </Button>
+                            <Message>
+                                You have an account ? <Link to='/Login'>Login</Link>
+                            </Message>
+                        </Segment>
+
+                        
+                    </Form>
+
+                </Grid.Column>
+            </Grid>
+        )
+    }
+
+}
+
+export default Register
